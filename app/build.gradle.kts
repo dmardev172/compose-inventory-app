60a0cbd4f966e3d451d354f27746f3672a472bf6
dmardev172 <dmardev172@gmail.com>
2025-11-26 19:18:46 +0100

    Set up dependencies for Hedgehog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,16 +17,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29"
-    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" //2.1.0-1.0.29
+//    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
+    namespace = "com.example.inventory"
     compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.inventory"
-        minSdk = 24
+        minSdk = 23 //24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -56,27 +57,29 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    namespace = "com.example.inventory"
 }
 
 dependencies {
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
-    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.02.01")) //2024.12.01
+    implementation("androidx.activity:activity-compose:1.8.2") //1.9.3
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0") //2.8.7
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0") //2.8.7
+    implementation("androidx.navigation:navigation-compose:2.8.0") //2.8.5
 
     // Testing
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0") //3.6.1
+    androidTestImplementation("androidx.test.ext:junit:1.3.0") //1.2.1
 }
